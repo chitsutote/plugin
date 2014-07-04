e7d4211eb59f05a283c63f64bb3b1b0b38f714d9
chitsutote <chitsutote7@gmail.com>
2014-07-05 07:47:36 +0800

    rename variable and fix changing episode malfunction when episode under 100

--- content.js
+++ content.js
@@ -21,22 +21,32 @@ window.onload = function () {
 			//Press B back to previous episode
 			if(event.keyCode == 66){
 
-				var url = document.URL.match(/http:\/\/comic.sfacg.com\/HTML\/HunterXHunter\//i);
+//				var url = document.URL.match(/http:\/\/comic.sfacg.com\/HTML\/HunterXHunter\//i);
+				var url = document.URL.match(/http:\/\/comic.sfacg.com\/HTML\/HZDLQ\//i);
 				var epi = document.URL.match(/\d+/);
-				var nextEp = parseInt(epi) - 1;
-
-				var nextUrl = url[0] + nextEp +"/#p=1";
+				var previousEp = parseInt(epi) - 1;
+				
+				if( previousEp < 100){
+					var nextUrl = url[0] + "0" + previousEp +"/#p=1";
+				}else{
+					var nextUrl = url[0] + previousEp + "/#p=1";
+				}
 				
 				setTimeout(function(){window.location.href=nextUrl;},20);
 			
 			//Press N forward to next episode
 			}else if(event.keyCode ==78){
 				
-				var url = document.URL.match(/http:\/\/comic.sfacg.com\/HTML\/HunterXHunter\//i);
+//				var url = document.URL.match(/http:\/\/comic.sfacg.com\/HTML\/HunterXHunter\//i);
+				var url = document.URL.match(/http:\/\/comic.sfacg.com\/HTML\/HZDLQ\//i);
 				var epi = document.URL.match(/\d+/);
 				var nextEp = parseInt(epi) + 1;
 
-				var nextUrl = url[0] + nextEp +"/#p=1";
+				if( nextEp < 100){
+					var nextUrl = url[0] + "0" + nextEp +"/#p=1";
+				}else{
+					var nextUrl = url[0] + nextEp + "/#p=1";
+				}
 				
 				setTimeout(function(){window.location.href=nextUrl;},20);
 			
@@ -47,7 +57,8 @@ window.onload = function () {
 		//Ctrl for next page  Shift for previous page
 		if(event.keyCode == 16 || event.keyCode == 17){
 			window.location.reload(true); 
-			var reg = document.URL.match(/http:\/\/comic.sfacg.com\/HTML\/HunterXHunter\/\d+\/#p=/i); 
+//			var reg = document.URL.match(/http:\/\/comic.sfacg.com\/HTML\/HunterXHunter\/\d+\/#p=/i); 
+			var reg = document.URL.match(/http:\/\/comic.sfacg.com\/HTML\/HZDLQ\/\d+\/#p=/i); 
 
 			var cut_length = reg[0].length; 
 			
