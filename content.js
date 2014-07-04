af4ab8b231e9a7f24aefaa5d7f710880fac564d9
chitsutote <chitsutote7@gmail.com>
2014-07-05 01:27:30 +0800

    Go to previous episode

--- content.js
+++ content.js
@@ -6,16 +6,53 @@ window.onload = function () {
 	
 	
 	document.onkeydown = function(){
+		
+		/* Test
+		window.location.reload();
+		var index = "http://comic.sfacg.com/HTML/" ;
+		var name = "HunterXHunter";
+		var epi = "345";
+		var url = index + name +"/"+epi;
+		window.location.assign(url);
+		window.location.reload();
+		*/
+		
+		if(event.keyCode ==66 || event.keyCode == 78){
+			alert(event.keyCode);	
+			//Press B back to previous episode
+			if(event.keyCode == 66){
+
+			//	window.location.reload(true);
+				var url = document.URL.match(/http:\/\/comic.sfacg.com\/HTML\/HunterXHunter\//i);
+				alert(url);
+				/*var test = document.URL.match(/\/\d+\//);
+				alert(test);*/
+				var epi = document.URL.match(/\d+/);
+				//alert(epi);
+				var nextEp = parseInt(epi) - 1;
+				alert(nextEp)
+
+				var nextUrl = url[0] + nextEp +"/";
+				alert(nextUrl);
+				
+				//window.location.assign(nextUrl);
+				setTimeout(function(){window.location.href=nextUrl;},20);
+			//	window.location.reload(true);
+			
+			//Press N forward to next episode
+			}else if(event.keyCode ==78){
+			
+			
+			}
+		}
+/*
 		if(event.keyCode == 16 || event.keyCode == 17){
 			window.location.reload(true); 
 			var reg = document.URL.match(/http:\/\/comic.sfacg.com\/HTML\/HunterXHunter\/\d+\/#p=/i); 
-			
-					
-			
+
 			var cut_length = reg[0].length; 
 			
 			
-		
 			//Parse out 
 			var page = document.URL.substring(cut_length); 
 			var page_int = parseInt(page); 
@@ -31,7 +68,7 @@ window.onload = function () {
 					window.location.replace(reg[0]+temp); 
 				} 
 					window.location.reload(true); 
-		}
+		}*/
 	};
 			
 		
